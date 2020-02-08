c151552f6c5172adecbb7645ce76f43ee825a658
unknown <verbo.camacho-v@labinfo.is.escuelaing.edu.co>
2020-02-08 08:26:03 -0500

    especificacion metodos

--- src/main/java/hangman/model/PowerScore.java
+++ src/main/java/hangman/model/PowerScore.java
@@ -3,6 +3,7 @@ package hangman.model;
 import hangman.exceptions.HangmanException;
 
 public class PowerScore implements GameScore {
+	
 	@Override
 	public int calculateScore(int correctCount, int incorrectCount) throws HangmanException {
 		// TODO Auto-generated method stub
