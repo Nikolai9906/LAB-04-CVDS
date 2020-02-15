4c1e4a00cb3866260d332533ea59e7586fb11962
Nikolai Bermudez <nicohhhh1@gmail.com>
2020-02-14 21:38:59 -0500

    Correccion de codigo

--- src/main/java/hangman/model/BonusScore.java
+++ src/main/java/hangman/model/BonusScore.java
@@ -4,17 +4,17 @@ import hangman.exceptions.HangmanException;
 
 public class BonusScore implements GameScore {
 
-	@Override
-	public int calculateScore(int correctCount, int incorrectCount) throws HangmanException {
-		 int score = 0 ;
-		 
-		 if (incorrectCount < 0 || correctCount < 0) { 
-			 throw new  HangmanException(HangmanException.INVALIDARGUMENT);
-		 }else if (incorrectCount >0) {
-			 score = score - (incorrectCount*5);
-		 }else if (correctCount > 0 ) {
-			 score = score + (correctCount*10);
-		 }
-		 return score;
-		 }
+	@Override 
+    public int calculateScore(int correctCount, int incorrectCount) throws HangmanException{
+        
+        if(correctCount < 0 || incorrectCount < 0){
+            throw new HangmanException(HangmanException.INVALIDARGUMENT);
+        }else if(incorrectCount < correctCount * 2){            
+            return (correctCount*10) - (incorrectCount*5);
+        }else if(incorrectCount >= correctCount *2){
+            return 0;
+        }
+        return 0;
+             
+    }
 }
